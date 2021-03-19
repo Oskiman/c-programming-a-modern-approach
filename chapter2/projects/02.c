b0e61cf6f2fc540d8f67e585eec464e9766ce81f
Oskiman <Oskiman@users.noreply.github.com>
2021-03-19 11:28:21 +0000

    Upload seems to have indented the top line and the return statement by 4 spaces?

--- chapter2/projects/02.c
+++ chapter2/projects/02.c
@@ -12,5 +12,6 @@ int main(void)
     const float pi = 3.14f;
     volume_of_sphere = (((4.0f / 3.0f) * pi) * radius_in_metres * radius_in_metres * radius_in_metres);
     printf("The volume of a sphere with a radius of %d is %.2f\n", radius_in_metres, volume_of_sphere);
-	return 0;
+	
+    return 0;
 }
