6ad4a0c872b0fe5cbf252cdcee81c41cca89f16a
Oskiman <Oskiman@users.noreply.github.com>
2021-03-19 11:30:25 +0000

    Update 06.c
    Upload seems to have indented the top line and the return statement by 4 spaces?

--- chapter2/projects/06.c
+++ chapter2/projects/06.c
@@ -15,5 +15,6 @@ int main(int argc, char **argv)
     //total = ((3 * (x * x * x * x * x) + (2 * (x * x * x * x)) - (5 * (x * x * x)) - (x * x) + (7 * x) - 6));
     total = ((((3 * x + 2) * x - 5) * x - 1) * x + 7)  * x - 6;
     printf("The total is: %.2f\n", total);
-	return 0;
+	
+    return 0;
 }
