6f7ff839a5b00d09a072bc2adef8a2abd317c8cb
Oskiman <Oskiman@users.noreply.github.com>
2021-03-20 17:45:06 +0000

    Add files via upload

---
+++ chapter4/projects/02.c
@@ -0,0 +1,22 @@
+/* C Programming A Modern Approach
+ * Chapter 4 Expressions 
+ * Programming Projects 2
+ * Extend the code from Programming Projects 1 to
+ * handle three digit numbers */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Enter a three-digit number: ");
+    int num = 0;
+    scanf("%d", &num);
+    int rnum3 = (num /100) % 10;    // last digit
+    int rnum1 = (num / 10) % 10;   // middle digit
+    int rnum2 = num % 10;   // first digit
+    
+    printf("The reversal is: %d%d%d\n", rnum2, rnum1, rnum3);
+    
+    
+	return 0;
+}
